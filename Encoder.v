3f645abbd42a4a16267d86821aa091b71674b264
DiyaJ <32401605+DiyaJ@users.noreply.github.com>
2018-11-04 13:30:33 +0100

    Add files via upload

--- Encoder.v
+++ Encoder.v
@@ -1,27 +1,25 @@
 `timescale 1ns / 1ps
 module Parity_Encoder(
-    input enable,
     input [31:0] data,
-    output[39:0] encoded_data,
+    output [5:0] parity,
+    output parity_DED
 );
 
-    wire parity[6:0];
-    
-    always @(posedge clk)
-        begin
-            if (enable == 1'b1)
-                assign parity[0] = data[0] xor data[1] xor data[3] xor data[4] xor data[6] xor data[8] xor data[10] xor data[11] xor data[13] xor data[15] xor data[17] xor data[19] xor data[21] xor data[23] xor data[25] xor data[26] xor data[28] xor data[30];
-                assign parity[1] = data[0] xor data[2] xor data[3] xor data[5] xor data[6] xor data[9] xor data[10] xor data[12] xor data[13] xor data[16] xor data[17] xor data[20] xor data[21] xor data[24] xor data[25] xor data[27] xor data[28] xor data[31];
-                assign parity[2] = data[1] xor data[2] xor data[3] xor data[7] xor data[8] xor data[9] xor data[10] xor data[14] xor data[15] xor data[16] xor data[17] xor data[22] xor data[23] xor data[24] xor data[25] xor data[29] xor data[30] xor data[31];
-                assign parity[3] = data[4] xor data[5] xor data[6] xor data[7] xor data[8] xor data[9] xor data[10] xor data[18] xor data[19] xor data[20] xor data[21] xor data[22] xor data[23] xor data[24] xor data[25];
-                assign parity[4] = data[11] xor data[12] xor data[13] xor data[14] xor data[15] xor data[16] xor data[17] xor data[18] xor data[19] xor data[20] xor data[21] xor data[22] xor data[23] xor data[24] xor data[25];
-                assign parity[5] = data[26] xor data[27] xor data[28] xor data[29] xor data[30] xor data[31]; 
-                assign parity[6] = data[0] xor data[1] xor data[3] xor data[4] xor data[6] xor data[7] xor data[8] xor data[9] xor data[10] xor data[11] xor data[12] xor data[13] xor data[14] xor data[15] xor data[16] xor data[17] xor data[18] xor data[19] xor data[20] xor data[21] xor data[22] xor data[23] xor data[24] xor data[25] xor data[26] xor data[27] xor data[28] xor data[29] xor data[30] xor data[31] xor parity[8] ;
-                
+begin
+                assign parity[0] = data[0] ^ data[1] ^ data[3] ^ data[4] ^ data[6] ^ data[8] ^ data[10] ^ data[11] ^ data[13] ^ data[15] ^ data[17] ^ data[19] 
+                                   ^ data[21] ^ data[23] ^ data[25] ^ data[26] ^ data[28] ^ data[30];
+                assign parity[1] = data[0] ^ data[2] ^ data[3] ^ data[5] ^ data[6] ^ data[9] ^ data[10] ^ data[12] ^ data[13] ^ data[16] ^ data[17] ^ data[20]
+                                   ^ data[21] ^ data[24] ^ data[25] ^ data[27] ^ data[28] ^ data[31];
+                assign parity[2] = data[1] ^ data[2] ^ data[3] ^ data[7] ^ data[8] ^ data[9] ^ data[10] ^ data[14] ^ data[15] ^ data[16] ^ data[17] ^ data[22] 
+                                   ^ data[23] ^ data[24] ^ data[25] ^ data[29] ^ data[30] ^ data[31];
+                assign parity[3] = data[4] ^ data[5] ^ data[6] ^ data[7] ^ data[8] ^ data[9] ^ data[10] ^ data[18] ^ data[19] ^ data[20] ^ data[21] ^ data[22] 
+                                   ^ data[23] ^ data[24] ^ data[25];
+                assign parity[4] = data[11] ^ data[12] ^ data[13] ^ data[14] ^ data[15] ^ data[16] ^ data[17] ^ data[18] ^ data[19] ^ data[20] ^ data[21] 
+                                   ^ data[22] ^ data[23] ^ data[24] ^ data[25];
+                assign parity[5] = data[26] ^ data[27] ^ data[28] ^ data[29] ^ data[30] ^ data[31]; 
+                assign parity_DED = data[0] ^ data[1] ^ data[2] ^ data[3] ^ data[4] ^ data[5] ^ data[6] ^ data[7] ^ data[8] ^ data[9] ^ data[10] ^ data[11] ^ data[12] ^ data[13] 
+                                   ^ data[14] ^ data[15] ^ data[16] ^ data[17] ^ data[18] ^ data[19] ^ data[20] ^ data[21] ^ data[22] ^ data[23] ^ data[24] ^ data[25] ^ data[26] ^ data[27] ^ data[28] ^ data[29] ^ data[30] ^ data[31] ^ parity[0] ^ parity[1] ^ parity[2] ^ parity[3] ^ parity[4] ^ parity[5];
+end      
+     
 
-
-        end   
-        
-    //assign q = q_out;
-    
 endmodule
---
+++ Encoder_tb.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+module Encoder_TB();
+reg [31:0] data;
+wire [5:0] parity;
+wire parity_DED;
+
+initial begin
+data = 32'h0;
+#5 
+data = 32'h1;
+#5 
+data = 32'h2;
+end 
+
+Parity_Encoder MUT(data, parity, parity_DED);  
+ 
+endmodule 
